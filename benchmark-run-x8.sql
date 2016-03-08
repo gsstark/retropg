8f7e5359b753e98cdbd17e84e67235ae2c6fe5d2
Greg Stark <stark@mit.edu>
2016-03-08 15:26:57 +0000

    add bigmem to normal x8 test and add eta to pgall

--- benchmark-run-x8.sql
+++ benchmark-run-x8.sql
@@ -23,7 +23,8 @@ select count(*) from x4;
 select count(*) from x8;
 checkpoint;
 \! sync
-\! sleep 5
+\! sync
+\! sleep 10
 select count(*) as warmup_x8 from t,x8;
 
 select count(*) as seqscan_x8 from t,x8;
@@ -35,20 +36,22 @@ select i as sort_text_x8    from t,x8 order by word  offset 11885295;
 select i as sort_binary_x8  from t,x8 order by wordb offset 11885295;
 select i as sort_integer_x8 from t,x8 order by i     offset 11885295;
 select i as sort_float_x8   from t,x8 order by f     offset 11885295;
+select i as sort_t100_x8    from t,x8 order by word  offset 99  limit 1;
 
 select i as sort_text_x8    from t,x8 order by word  offset 11885295;
 select i as sort_binary_x8  from t,x8 order by wordb offset 11885295;
 select i as sort_integer_x8 from t,x8 order by i     offset 11885295;
 select i as sort_float_x8   from t,x8 order by f     offset 11885295;
-
-select i as sort_integer_x8 from t,x8 order by i     offset 11885295;
-select i as sort_float_x8   from t,x8 order by f     offset 11885295;
-select i as sort_text_x8    from t,x8 order by word  offset 11885295;
-select i as sort_binary_x8  from t,x8 order by wordb offset 11885295;
-
-select i as sort_t100_x8  from t,x8 order by word  offset 99  limit 1;
-select i as sort_t100_x8  from t,x8 order by word  offset 99  limit 1;
-select i as sort_t100_x8  from t,x8 order by word  offset 99  limit 1;
+select i as sort_t100_x8    from t,x8 order by word  offset 99  limit 1;
 
 select count(*) as seqscan_x8 from t,x8;
 select count(*) as seqscan_x8 from t,x8;
+
+set work_mem=8192000;
+set sort_mem=8192000;
+
+show work_mem;
+show sort_mem;
+
+select i as sort_text_x8_bigmem    from t,x8 order by word  offset 11885295;
+select i as sort_text_x8_bigmem    from t,x8 order by word  offset 11885295;
